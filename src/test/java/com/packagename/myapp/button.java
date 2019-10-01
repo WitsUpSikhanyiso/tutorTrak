41c54b11bceb9953c595eb8e50e1cac6a0fb87bc
WitsUpSikhanyiso <1428523@students.wits.ac.za>
2019-10-01 13:40:27 +0200

    Create button.java

---
+++ src/test/java/com/packagename/myapp/button.java
@@ -0,0 +1,9 @@
+package com.packagename.myapp;
+import java.util.HashMap;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.packagename.app.Mainview;
+import com.vaadin.ui.Button;
