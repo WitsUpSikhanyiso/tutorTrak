dd1ed7169e995f31308ed8035e63b0a55657c25a
WitsUpSikhanyiso <1428523@students.wits.ac.za>
2019-10-03 14:49:25 +0200

    Update AbstractViewTest.java

--- src/test/java/com/packagename/myapp/AbstractViewTest.java
+++ src/test/java/com/packagename/myapp/AbstractViewTest.java
@@ -2,10 +2,6 @@ package com.packagename.myapp;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
-
-
-
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
